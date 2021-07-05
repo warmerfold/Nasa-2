9e10a4a05fb8ae078c75fb98f66eea0e116f31e5
ravindrakumar9 <84221874+warmerfold@users.noreply.github.com>
2021-07-05 11:04:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Open */
+function openNav() {
+ document.getElementById("myNav")
+  .style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+ document.getElementById("myNav")
+  .style.height = "0%";
+} <
+script async src =
+ "https://cse.google.com/cse.js?cx=YOUR_ENGINE_ID" >
+ <
+ /script> <
+div class =
+ "gcse-searchresults-only" > < /div>
+var feed = ”
+ https: //www.nasa.gov/rss/dyn/breaking_news.rss”;
